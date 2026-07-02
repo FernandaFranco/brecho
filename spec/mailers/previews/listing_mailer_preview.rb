5a2340fbf987cdbf50e61a7a55346ee6b4111c99
Fernanda Franco <francodias.fernanda@gmail.com>
2026-07-02 13:14:03 -0300

    Atualiza CI

--- spec/mailers/previews/listing_mailer_preview.rb
+++ spec/mailers/previews/listing_mailer_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/listing_mailer_mailer
 class ListingMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/listing_mailer_mailer/new_listing_notification
   def new_listing_notification
     ListingMailer.new_listing_notification
   end
-
 end
