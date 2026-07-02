f706c42afb65eadfe721227b19eb7ada60179518
Fernanda Franco <francodias.fernanda@gmail.com>
2026-07-02 12:06:23 -0300

    Fase 7: Cache com Solid Cache e Russian Doll Caching

--- app/javascript/channels/listing_channel.js
+++ app/javascript/channels/listing_channel.js
@@ -1,16 +1,21 @@
 import consumer from "channels/consumer";
 
-consumer.subscriptions.create(
-  {
-    channel: "ListingChannel",
-    id: document.querySelector("[data-listing-id]")?.dataset.listingId,
-  },
-  {
-    connected() {},
-    disconnected() {},
-    received(data) {
-      const counter = document.querySelector("[data-views-count]");
-      if (counter) counter.textContent = data.views_count;
+const listingId =
+  document.querySelector("[data-listing-id]")?.dataset.listingId;
+
+if (listingId) {
+  consumer.subscriptions.create(
+    {
+      channel: "ListingChannel",
+      id: document.querySelector("[data-listing-id]")?.dataset.listingId,
     },
-  },
-);
+    {
+      connected() {},
+      disconnected() {},
+      received(data) {
+        const counter = document.querySelector("[data-views-count]");
+        if (counter) counter.textContent = data.views_count;
+      },
+    },
+  );
+}
